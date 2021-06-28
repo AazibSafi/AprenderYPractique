4aace4471a4059e5a3d6bd3c67fb4089fd50e67b
Aazib Safi <aazibsafi@gmail.com>
2021-06-28 16:55:30 -0400

    Kth Permutations

---
+++ src/test/java/com/algorithms/aprenderypractique/algorithm/KthPermutation_Integer.java
@@ -0,0 +1,88 @@
+package com.algorithms.aprenderypractique.algorithm;
+
+import com.algorithms.aprenderypractique.BaseTest;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    For Integers
+ * https://www.youtube.com/watch?v=W9SIlE2jhBQ
+*/
+
+public class KthPermutation_Integer extends BaseTest {
+
+    @Test
+    public void test() {
+        int sequence = 1234;
+        System.out.println(findKthPermutation(sequence,12));
+    }
+
+/*
+        loop through size of list till n-1
+            get the index by formula
+            check the boundary of index
+            get the item from list at index
+            add that item into finalResult
+            remove that item from sequence list
+            decrease size of sequence
+            update the K val
+        add the last item from the list into the finalResult
+
+        Total Permutation -> n!
+        Block Size -> (n-1)!
+        Index -> k/(n-1)!
+ */
+
+    public Integer findKthPermutation(Integer sequence, int k) {
+
+        List<Integer> sequenceList = breakTheSequence(sequence);
+        int n = sequenceList.size();
+        Integer kthPermutation = 0;
+
+        while(n > 1) {
+            int index = k/fact(n-1);
+            if(k % fact(n-1) == 0) {
+                index--;
+            }
+
+            kthPermutation = addInTheResultPermutation(kthPermutation, sequenceList.get(index) );
+
+            sequenceList.remove(index);
+
+            k = k - fact(n-1)*index;
+            n--;
+        }
+
+        kthPermutation = addInTheResultPermutation(kthPermutation, sequenceList.get(0) );
+
+        return kthPermutation;
+    }
+
+    private Integer addInTheResultPermutation(Integer kthPermutation, Integer item) {
+        if(kthPermutation == null ) {
+            kthPermutation = 0;
+        }
+        return kthPermutation*10 + item;
+    }
+
+    private List<Integer> breakTheSequence(Integer sequence) {
+        List<Integer> sequenceList = new ArrayList<>();
+        while(sequence != 0) {
+            int remainder = sequence % 10;
+            sequenceList.add(0,remainder);
+
+            sequence /= 10;
+        }
+        return sequenceList;
+    }
+
+    private int fact(int x) {
+        if(x == 0 || x == 1) {
+            return 1;
+        }
+        return x*fact(x-1);
+    }
+
+}
---
+++ src/test/java/com/algorithms/aprenderypractique/algorithm/KthPermutation_String.java
@@ -0,0 +1,78 @@
+package com.algorithms.aprenderypractique.algorithm;
+
+import com.algorithms.aprenderypractique.BaseTest;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * For Strings
+ * https://www.youtube.com/watch?v=W9SIlE2jhBQ
+ */
+public class KthPermutation_String extends BaseTest {
+
+    @Test
+    public void test() {
+        String sequence = "ABCD";
+        System.out.println(findKthPermutation(sequence,6));
+    }
+
+/*
+    loop through size of list till n-1
+        get the index by formula
+        check the boundary of index
+        get the item from list at index
+        add that item into finalResult
+        remove that item from sequence list
+        decrease size of sequence
+        update the K val
+    add the last item from the list into the finalResult
+
+    Total Permutation -> n!
+    Block Size -> (n-1)!
+    Index -> k/(n-1)!
+ */
+    public String findKthPermutation(String sequence, int k) {
+
+        List<Character> sequenceList = breakTheSequence(sequence);
+        int n = sequenceList.size();
+        StringBuilder kthPermutation = new StringBuilder();
+
+        while(n > 1) {
+            int index = k/fact(n-1);
+            if(k % fact(n-1) == 0) {
+                index--;
+            }
+
+            kthPermutation = addInTheResultPermutation(kthPermutation, sequenceList.get(index) );
+
+            sequenceList.remove(index);
+
+            k = k - fact(n-1)*index;
+            n--;
+        }
+
+        kthPermutation = addInTheResultPermutation(kthPermutation, sequenceList.get(0));
+
+        return kthPermutation.toString();
+    }
+
+    private StringBuilder addInTheResultPermutation(StringBuilder kthPermutation, Character item) {
+        kthPermutation.append(item);
+        return kthPermutation;
+    }
+
+
+    private List<Character> breakTheSequence(String sequence) {
+        return sequence.chars().mapToObj(elm -> (char) elm).collect(Collectors.toList());
+    }
+
+    private int fact(int x) {
+        if(x == 0 || x == 1) {
+            return 1;
+        }
+        return x*fact(x-1);
+    }
+
+}
